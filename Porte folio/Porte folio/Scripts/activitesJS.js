588611997c6e2d6dd55b8566688e659149b7bd1a
loutyl <t.maalem@aforp.eu>
2015-01-20 09:22:06 +0100

    Ajout dossier projet porte folio
    Ajout du dossier de projet porte folio

---
+++ Porte folio/Porte folio/Scripts/activitesJS.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $('[data-toggle="popover"]').popover({
+        placement: 'top'
+    });
+
+    $('#popMCD').popover("destroy").popover({
+        content: "Modèle Conceptuel de données"
+    });
+    
+    $('#popMLD').popover("destroy").popover({
+        content: "Modèle Logique de Données"
+    });
+
+    $('#popMPD').popover("destroy").popover({
+        content: "Modèle Physique de données"
+    });
+
+    $('#popHTML').popover("destroy").popover({
+        content: "HTML or HyperText Markup Language is the standard markup language "+
+        "used to create Web pages. HTML is written in the form of HTML elements "+
+        "consisting of tags enclosed in angle brackets (like <html> )."
+    });
+
+    $('#popCSS').popover("destroy").popover({
+        content: "Cascading Style Sheets (CSS) is a style sheet language used for "+
+        "describing the look and formatting of a document written in a markup language."
+    });
+
+});
